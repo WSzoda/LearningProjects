6a940ec9e5100a2f259a100e5c41fe02dda8d683
Wojciech Szoda <wojciechszoda@icloud.com>
2024-01-04 19:59:23 +0100

    Game 2 done

--- Pop a baloon/Assets/Scripts/BalloonController.cs
+++ Pop a baloon/Assets/Scripts/BalloonController.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
-public class BaloonController : MonoBehaviour
+public class BalloonController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public TextMeshProUGUI text;
+    private int _score = 0;
+    private AudioSource _audioSource;
+
+    private void Awake()
     {
-        
+        _audioSource = GetComponent<AudioSource>();
+    }
+    
+    private void FixedUpdate()
+    {
+        transform.Translate(0, speed, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        _score++;
+        text.text = _score.ToString();
+        _audioSource.Play();
+
+        speed += 0.01f;
+        ResetPosition();
+    }
+
+    private void ResetPosition()
+    {
+        var xPosition = Random.Range(-2.5f, 2.5f);
+        transform.position = new Vector2(xPosition, -6);
     }
 }
