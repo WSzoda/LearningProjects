97a560562c68e2155fa401eb2057bd3794462a8d
Wojciech Szoda <wojciechszoda@icloud.com>
2024-01-05 13:24:30 +0100

    Obstacle now move. Player can jump ony while standing on ground

--- Cube Runner/Assets/Scripts/PlayerController.cs
+++ Cube Runner/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce;
+    private Rigidbody _rb;
+    private bool _canJump;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!_canJump)
+            return;
         
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _canJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _canJump = false;
+        }
     }
 }
