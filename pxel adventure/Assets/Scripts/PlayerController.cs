c29a6daf1cc7e0f22ef5443a70b5a412bc127a62
Wojciech Szoda <wojciechszoda@icloud.com>
2024-01-08 14:46:47 +0100

    Added option to jump

--- pxel adventure/Assets/Scripts/PlayerController.cs
+++ pxel adventure/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float jumpForce;
+    
+    private Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Jump()
+    {
+        _rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
 }
